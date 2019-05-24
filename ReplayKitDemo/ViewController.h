59c4949e97127f05fc29badbcabf9a63d3293bb6
ydd <yuedd@ibobei.cn>
2019-05-24 10:42:10 +0800

    Initial Commit

---
+++ ReplayKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReplayKitDemo
+//
+//  Created by ydd on 2019/5/24.
+//  Copyright © 2019 ydd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReplayKitDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ReplayKitDemo
+//
+//  Created by ydd on 2019/5/24.
+//  Copyright © 2019 ydd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
